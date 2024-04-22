77c1be01e7e46a72b25268479afd209b2497a809
xPasquale1 <38603532+xPasquale1@users.noreply.github.com>
2024-04-22 11:11:22 +0200

    Kollisionstest wieder drinnen, da Fehler sonst

--- main.cpp
+++ main.cpp
@@ -153,42 +153,6 @@ void processNetworkPackets()noexcept{
     }
 }
 
-/// @brief Schreibt Werte vom Image original linear interpoliert in das Image interpolated
-/// @param original Das original Image
-/// @param interpolated Das resultierende interpolierte Image
-void interpolateHeatmap(Image& original, Image& interpolated)noexcept{
-    float pixelsX = (float)(interpolated.width)/(original.width-1);
-    float pixelsY = (float)(interpolated.height)/(original.height-1);
-    for(WORD pty=0; pty < original.height-1; ++pty){
-        for(WORD ptx=0; ptx < original.width-1; ++ptx){
-            DWORD startValX0 = original.data[pty*original.width+ptx];
-            DWORD endValX0 = original.data[pty*original.width+ptx+1];
-            DWORD startValX1 = original.data[(pty+1)*original.width+ptx];
-            DWORD endValX1 = original.data[(pty+1)*original.width+ptx+1];
-            WORD startX = ptx*pixelsX;
-            WORD endX = (ptx+1)*pixelsX;
-            WORD startY = pty*pixelsY;
-            WORD endY = (pty+1)*pixelsY;
-            for(WORD x=startX; x < endX; ++x){
-                float t = (float)(x - startX)/(endX - startX);
-                BYTE red0 = R(startValX0) + (R(endValX0) - R(startValX0))*t;
-                BYTE green0 = G(startValX0) + (G(endValX0) - G(startValX0))*t;
-                BYTE blue0 = B(startValX0) + (B(endValX0) - B(startValX0))*t;
-                BYTE red1 = R(startValX1) + (R(endValX1) - R(startValX1))*t;
-                BYTE green1 = G(startValX1) + (G(endValX1) - G(startValX1))*t;
-                BYTE blue1 = B(startValX1) + (B(endValX1) - B(startValX1))*t;
-                for(WORD y=startY; y < endY; ++y){
-                    t = (float)(y - startY)/(endY - startY);
-                    BYTE red = red0 + (red1 - red0)*t;
-                    BYTE green = green0 + (green1 - green0)*t;
-                    BYTE blue = blue0 + (blue1 - blue0)*t;
-                    interpolated.data[y*interpolated.width+x] = RGBA(red, green, blue);
-                }
-            }
-        }
-    }
-}
-
 //Zeichnet ein linear interpoliertes Dreick in das Image image, mit den Eckpunkten (x1, y1),... und den Eckpunktfarben val1,...
 void drawInterpolatedTriangle(Image& image, WORD x1, WORD y1, WORD x2, WORD y2, WORD x3, WORD y3, BYTE val1, BYTE val2, BYTE val3)noexcept{
     WORD minX = std::min(x1, std::min(x2, x3));
@@ -198,20 +162,76 @@ void drawInterpolatedTriangle(Image& image, WORD x1, WORD y1, WORD x2, WORD y2, 
 
     float totalArea = 1.f/((x2-x1)*(y3-y2)-(y2-y1)*(x3-x2));
 
-    // DWORD color = getColor();
-
     for(WORD y=minY; y <= maxY; ++y){
         for(WORD x=minX; x <= maxX; ++x){
             float m1 = ((x2-x)*(y3-y)-(x3-x)*(y2-y))*totalArea;
             float m2 = ((x3-x)*(y1-y)-(x1-x)*(y3-y))*totalArea;     //TODO können inkremental in jeder Schleife berechnet werden
             float m3 = 1-m1-m2;
-            if(m1 >= -0.001 && m2 >= -0.001 && m3 >= -0.001){   //TODO meh
+            if(m1 >= 0 && m2 >= 0 && m3 >= 0){   //TODO Funktioniert noch nicht ganz korrekt, vllt kann man float Zahlen vermeiden?
+                // m1 = m1*m1;
+                // m2 = m2*m2;
+                // m3 = m3*m3;
                 BYTE value = val1*m1 + val2*m2 + val3*m3;
                 image.data[y*image.width+x] = RGBA(value, 255-value, 0);
-                // image.data[y*image.width+x] = color;
             }
         }
     }
+}
+
+bool lineSegmentIntersection(WORD xBeg1, WORD yBeg1, WORD xEnd1, WORD yEnd1, WORD xBeg2, WORD yBeg2, WORD xEnd2, WORD yEnd2, DWORD& identical)noexcept{
+    if(xBeg1 == xBeg2 && yBeg1 == yBeg2 && xEnd1 == xEnd2 && yEnd1 == yEnd2){   //Linien sind identisch
+        identical++;
+        return false;
+    }
+    if(xBeg1 == xEnd2 && yBeg1 == yEnd2 && xEnd1 == xBeg2 && yEnd1 == yBeg2){
+        identical++;
+        return false;
+    }
+    int dx1 = xEnd1 - xBeg1;
+    int dy1 = yEnd1 - yBeg1;
+    int dx2 = xEnd2 - xBeg2;
+    int dy2 = yEnd2 - yBeg2;
+
+    int denom = dy2*dx1-dx2*dy1;
+    if(denom == 0){     //Linien parallel, teste ob einer der Punkte in der jeweils anderen Linie liegt
+        float tx1 = (xBeg2 - xBeg1)/(float)dx1;
+        float ty1 = (yBeg2 - yBeg1)/(float)dy1;
+        float tx2 = (xEnd2 - xBeg1)/(float)dx1;
+        float ty2 = (yEnd2 - yBeg1)/(float)dy1;
+        float tx3 = (xBeg1 - xBeg2)/(float)dx2;
+        float ty3 = (yBeg1 - yBeg2)/(float)dy2;
+        float tx4 = (xEnd1 - xBeg2)/(float)dx2;
+        float ty4 = (yEnd1 - yBeg2)/(float)dy2;
+        if(tx1 > 0.001 && tx1 < 0.999 && ty1 > 0.001 && ty1 < 0.999) return true;
+        if(tx2 > 0.001 && tx2 < 0.999 && ty2 > 0.001 && ty2 < 0.999) return true;
+        if(tx3 > 0.001 && tx3 < 0.999 && ty3 > 0.001 && ty3 < 0.999) return true;
+        if(tx4 > 0.001 && tx4 < 0.999 && ty4 > 0.001 && ty4 < 0.999) return true;
+        return false;
+    }
+
+    int dxBeg = xBeg2-xBeg1;
+    int dyBeg = yBeg2-yBeg1;
+
+    float t1 = (dy2*dxBeg-dx2*dyBeg)/(float)denom;
+    float t2 = (dy1*dxBeg-dx1*dyBeg)/(float)denom;
+
+    return (t1 > 0 && t1 < 1 && t2 > 0 && t2 < 1);  //TODO ist noch nicht ganz richtig, es könnte ja ein Endpunkt auf einer Kante liegen, was okay ist
+}
+
+bool triangleOverlap(WORD x11, WORD y11, WORD x12, WORD y12, WORD x13, WORD y13, WORD x21, WORD y21, WORD x22, WORD y22, WORD x23, WORD y23, DWORD& identical)noexcept{
+    if(lineSegmentIntersection(x11, y11, x12, y12, x21, y21, x22, y22, identical)) return true;
+    if(lineSegmentIntersection(x11, y11, x13, y13, x21, y21, x22, y22, identical)) return true;
+    if(lineSegmentIntersection(x12, y12, x13, y13, x21, y21, x22, y22, identical)) return true;
+
+    if(lineSegmentIntersection(x11, y11, x12, y12, x21, y21, x23, y23, identical)) return true;
+    if(lineSegmentIntersection(x11, y11, x13, y13, x21, y21, x23, y23, identical)) return true;
+    if(lineSegmentIntersection(x12, y12, x13, y13, x21, y21, x23, y23, identical)) return true;
+
+    if(lineSegmentIntersection(x11, y11, x12, y12, x22, y22, x23, y23, identical)) return true;
+    if(lineSegmentIntersection(x11, y11, x13, y13, x22, y22, x23, y23, identical)) return true;
+    if(lineSegmentIntersection(x12, y12, x13, y13, x22, y22, x23, y23, identical)) return true;
+
+    return false;
 }
 
 struct DatapointTriangle{
@@ -221,6 +241,8 @@ struct DatapointTriangle{
 };
 
 //TODO O(n^4) brute force Delauney Umsetzung, es gibt deutlich effizientere Methoden
+
+// #define VISUALIZETRIANGULATION
 
 /// @brief Trianguliert eine Punktewolke von Datapoints, interpoliert die Werte dazwischen und füllt damit das Image aus heatmaps am Index heatmapIdx
 /// @param heatmaps Ein Array aus Images
@@ -246,6 +268,9 @@ void interpolateTriangulation(Image* heatmaps, BYTE heatmapIdx, Datapoint* datap
                 if(k == j || k == i) continue;
                 Datapoint& p3 = scaledDatapoints[k];
                 bool valid = true;
+
+                float totalArea = (p2.x-p1.x)*(p3.y-p2.y)-(p2.y-p1.y)*(p3.x-p2.x);
+                if(totalArea <= 0) continue;
 
                 float midXP1P2 = (p1.x+p2.x)/2;
                 float midYP1P2 = (p1.y+p2.y)/2;
@@ -273,7 +298,35 @@ void interpolateTriangulation(Image* heatmaps, BYTE heatmapIdx, Datapoint* datap
                         break;
                     }
                 }
-                if(!valid) continue;       //TODO muss man noch auf Dreiecksintersection testen?
+                if(!valid) continue;
+                DWORD identical = 0;
+                for(size_t l=0; l < triangles.size(); ++l){     //Testet ob das Dreieck mit anderen überlappt
+                    DatapointTriangle& tri = triangles[l];
+                    if(triangleOverlap(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y, tri.d1->x, tri.d1->y, tri.d2->x, tri.d2->y, tri.d3->x, tri.d3->y, identical)){
+                        valid = false;
+                        break;
+                    }
+                }
+                if(!valid) continue;    //TODO identical muss vllt auch noch geprüft werden
+
+                #ifdef VISUALIZETRIANGULATION
+                clearWindow(window);
+                for(DWORD l=0; l < count; ++l){
+                    drawRectangle(window, scaledDatapoints[l].x+200-2, scaledDatapoints[l].y-2, 4, 4, RGBA(255, 255, 255));
+                }
+                for(size_t l=0; l < triangles.size(); ++l){
+                    DatapointTriangle& tri = triangles[l];
+                    drawLine(window, tri.d1->x+200, tri.d1->y, tri.d2->x+200, tri.d2->y, RGBA(255, 255, (l+1)/triangles.size()));
+                    drawLine(window, tri.d1->x+200, tri.d1->y, tri.d3->x+200, tri.d3->y, RGBA(255, 255, (l+1)/triangles.size()));
+                    drawLine(window, tri.d3->x+200, tri.d3->y, tri.d2->x+200, tri.d2->y, RGBA(255, 255, (l+1)/triangles.size()));
+                }
+                drawLine(window, p1.x+200, p1.y, p2.x+200, p2.y, RGBA(255, 0, 0));
+                drawLine(window, p1.x+200, p1.y, p3.x+200, p3.y, RGBA(255, 0, 0));
+                drawLine(window, p3.x+200, p3.y, p2.x+200, p2.y, RGBA(255, 0, 0));
+                drawWindow(window);
+                getchar();
+                #endif
+
                 triangles.push_back({&p1, &p2, &p3});
             }
         }
