1b1a3ce30e02633781c818eac5fbbd74884c43bd
xPasquale1 <38603532+xPasquale1@users.noreply.github.com>
2025-04-09 13:52:06 +0200

    59 Messpunkte AVG

--- distance.py
+++ distance.py
@@ -10,6 +10,6 @@ def calculate_stats(numbers):
 
     return avg, std_dev
 
-numbers = [0.251, 1.534, 1.247, 0.470, 3.165, 0.603, 1.867, 0.318, 0.722, 0.959, 0.879]
+numbers = [1.557, 3.746, 2.899, 4.389, 2.823, 1.880, 2.694, 0.444, 1.136, 2.677, 2.256]
 avg, std_dev = calculate_stats(numbers)
 print(f"Durchschnitt: {avg:.3f}, Standardabweichung: {std_dev:.3f}")
