938f6deb37c6b02e039fb16b5948d252dee4af13
xPasquale1 <38603532+xPasquale1@users.noreply.github.com>
2025-04-22 15:43:20 +0200

    Auswertung Stockwerk

--- distance.py
+++ distance.py
@@ -10,6 +10,6 @@ def calculate_stats(numbers):
 
     return avg, std_dev
 
-numbers = [0.945, 1.634, 1.084, 3.145, 1.479, 2.642, 1.151, 4.027]
+numbers = [1.160, 0.696, 0.357, 0.299, 0.338, 2.224, 1.537, 0.676, 1.711, 1.286]
 avg, std_dev = calculate_stats(numbers)
 print(f"Durchschnitt: {avg:.3f}, Standardabweichung: {std_dev:.3f}")
